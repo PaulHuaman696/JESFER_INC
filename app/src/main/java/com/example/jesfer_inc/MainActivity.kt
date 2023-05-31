dd0c15ee6be6c7dfc94a96dc807ed11ffb74a471
TheBeatboxer <efercitolove@gmail.com>
2023-05-31 14:27:29 -0500

    Menu listo

--- app/src/main/java/com/example/jesfer_inc/MainActivity.kt
+++ app/src/main/java/com/example/jesfer_inc/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.jesfer_inc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val entramenu = findViewById<Button>(R.id.btn_entra) //Colocas el ID del boton
+        entramenu.setOnClickListener{
+            entrarMenu()
+        }
+    }
+
+    private fun entrarMenu(){
+        val i = Intent(this, MainActivity2::class.java)
+        startActivity(i)
     }
 }
